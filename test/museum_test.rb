bbfe927941de2faab819ada49083d3ffe332d2c8
George Soderholm <georgesoderholm@gmail.com>
2020-09-10 09:27:41 -0600

    Add test to see if the attribute is there

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -8,6 +8,12 @@ class MuseumTest < Minitest::Test
   def test_it_exists
     dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_instance_of Museum, dmns 
+    assert_instance_of Museum, dmns
+  end
+
+  def test_it_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
   end
 end
