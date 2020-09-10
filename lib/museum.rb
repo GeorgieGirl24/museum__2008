05944baa85fdd6070147e410f0a010d23b233808
George Soderholm <georgesoderholm@gmail.com>
2020-09-10 09:56:31 -0600

    Add a method to add patrons to an array

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,11 +1,28 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
     @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    recommended_exhibits = []
+    @exhibits.each do |exhibit|
+      patron.interests.select do |interest|
+        if exhibit.name == interest
+          recommended_exhibits << exhibit
+        end
+      end
+    end
+    recommended_exhibits
+  end
+
+  def admit(patron)
+    @patrons << patron 
   end
 end
